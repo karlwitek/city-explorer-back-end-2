453fa27b15d575184d1179f60671343a9abe1f89
karlwitek <karl.witek20@gmail.com>
2020-05-06 11:04:13 -0700

    set up plus some index code

---
+++ index.js
@@ -0,0 +1,16 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const express = require('express');
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.json({hello: 'world'});
+});
+
+app.listen(PORT, () => console.log(`running on port ${PORT}`));
+
